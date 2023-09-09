b0914953db8ff1e936f3675b1072afff195977fc
Kent C. Dodds <me+github@kentcdodds.com>
2023-09-09 12:49:09 -0600

    fix unique in connections model

---
+++ exercises/18.provider-errors/01.solution.auth-error/prisma/migrations/20230909183517_connections/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Connection" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "providerName" TEXT NOT NULL,
+    "providerId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Connection_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Connection_providerName_providerId_key" ON "Connection"("providerName", "providerId");
