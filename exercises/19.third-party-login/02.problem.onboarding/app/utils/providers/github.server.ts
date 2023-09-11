17c725159967800155fe28e467f74f236e6b8ced
Kent C. Dodds <me+github@kentcdodds.com>
2023-09-11 16:16:54 -0600

    fix github image

--- exercises/19.third-party-login/02.problem.onboarding/app/utils/providers/github.server.ts
+++ exercises/19.third-party-login/02.problem.onboarding/app/utils/providers/github.server.ts
@@ -59,7 +59,8 @@ export class GitHubProvider implements AuthProvider {
 		)
 		const state = cuid()
 		connectionSession.set('oauth2:state', state)
-		const code = 'MOCK_GITHUB_CODE_KODY'
+		// 🦉 Change this code any time you want to test a new GitHub user:
+		const code = 'MOCK_GITHUB_CODE_HANNAH'
 		const searchParams = new URLSearchParams({ code, state })
 		throw redirect(`/auth/github/callback?${searchParams}`, {
 			headers: {
