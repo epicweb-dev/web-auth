6a6ef220f19872a0a37720ebfd0fd269271f21af
Joshua Stafman <github@jstafman.com>
2023-11-20 21:18:38 -0500

    Add csrf validation to logout action

--- exercises/06.logout/01.solution.logout/app/routes/_auth+/logout.tsx
+++ exercises/06.logout/01.solution.logout/app/routes/_auth+/logout.tsx
@@ -1,4 +1,5 @@
 import { type DataFunctionArgs, redirect } from '@remix-run/node'
+import { validateCSRF } from '#app/utils/csrf.server.ts'
 import { sessionStorage } from '#app/utils/session.server.ts'
 
 export async function loader() {
@@ -6,6 +7,8 @@ export async function loader() {
 }
 
 export async function action({ request }: DataFunctionArgs) {
+	const formData = await request.formData()
+	await validateCSRF(formData, request.headers)
 	const cookieSession = await sessionStorage.getSession(
 		request.headers.get('cookie'),
 	)
