6d17edadded777f44496b15df637ea446d85dfcb
Michael Tedla <mickykebe@gmail.com>
2024-01-02 18:49:56 -0600

    fix typo in auth.server.ts (#28)

--- exercises/09.managed-sessions/02.problem.auth-utils/app/utils/auth.server.ts
+++ exercises/09.managed-sessions/02.problem.auth-utils/app/utils/auth.server.ts
@@ -21,7 +21,7 @@ export async function getUserId(request: Request) {
 	// 🐨 this isn't a userId anymore, it's a sessionId
 	const userId = cookieSession.get(userIdKey)
 	if (!userId) return null
-	// 🐨 query the sessionId table instead. Do a subquery to get the user id
+	// 🐨 query the session table instead. Do a subquery to get the user id
 	// 💰 make sure to only select sessions that have not yet expired!
 	// 📜 https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#gt
 	const user = await prisma.user.findUnique({
